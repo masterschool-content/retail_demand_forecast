3f56ee7174a5c8dbbc2a09ffd0ce457f71a17fb3
masterschool-content <da.content@masterschool.com>
2024-10-31 18:36:46 +0000

    add init files

--- app/config.py
+++ app/config.py
@@ -2,6 +2,12 @@
 
 DATA_PATH = "./data/"
 MODEL_URI = "models:/YourModelName/Production"
+your_file_id_for_stores_csv = '1YgE0K5mrlsHg8QmWnozuAsX9YPk4EYgf'
+your_file_id_for_items_csv = '1gsts50e_UfBPonkAg67VtLE44zG85It4'
+your_file_id_for_transactions_csv = '1-4RSVKRYYlnIY4Cd-G_ZYbkJqt-rPXVx'
+your_file_id_for_oil_csv = '1QDzb7nsVN_1qISS6iTBel4gwlYNtrMjU'
+your_file_id_for_holidays_csv = '1asSLMupocsH8CEoykirb74BwppGNNQLG'
+your_file_id_for_train_csv = '1-2t5PWG6ngtqNMCwi6Nyd3IAGl_1__eA'
 
 # Google Drive links for each file (replace with actual file IDs)
 GOOGLE_DRIVE_LINKS = {
